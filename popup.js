630573dc8596fd16e3716aed9fe7abaaa46238f3
AnderGI <75628843+AnderGI@users.noreply.github.com>
2025-02-23 10:09:40 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,24 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var currentTab = tabs[0];
-        var actionButton = document.getElementById('actionButton');
-        var downloadCsvButton = document.getElementById('downloadCsvButton');
-        var resultsTable = document.getElementById('resultsTable');
-        var filenameInput = document.getElementById('filenameInput');
+        const currentTab = tabs[0];
+        const actionButton = document.getElementById('actionButton');
+        const downloadCsvButton = document.getElementById('downloadCsvButton');
+        const resultsTable = document.getElementById('resultsTable');
+        const filenameInput = document.getElementById('filenameInput');
+        const messageEl = document.getElementById('message')
 
         if (currentTab && currentTab.url.includes("://www.google.com/maps/search")) {
-            document.getElementById('message').textContent = "Let's scrape Google Maps!";
+            const DEFAULT_MESSAGE_TEXT = "Let's scrape Google Maps!"
+            messageEl.textContent = DEFAULT_MESSAGE_TEXT;
             actionButton.disabled = false;
             actionButton.classList.add('enabled');
         } else {
-            var messageElement = document.getElementById('message');
-            messageElement.innerHTML = '';
+            
+            messageEl.innerHTML = '';
             var linkElement = document.createElement('a');
-            linkElement.href = 'https://www.google.com/maps/search/';
-            linkElement.textContent = "Go to Google Maps Search.";
+            const GOOGLE_MAPS_LINK = 'https://www.google.com/maps/search/';
+            linkElement.href = GOOGLE_MAPS_LINK;
+            const GO_TO_GOOGLE_MAPS_TEXT = "Go to Google Maps Search."
+            linkElement.textContent = GO_TO_GOOGLE_MAPS_TEXT;
             linkElement.target = '_blank'; 
-            messageElement.appendChild(linkElement);
-
+            messageEl.appendChild(linkElement);
             actionButton.style.display = 'none'; 
             downloadCsvButton.style.display = 'none';
             filenameInput.style.display = 'none'; 
@@ -34,29 +37,27 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
 
                 // Define and add headers to the table
-                const headers = ['Title', 'Rating', 'Reviews', 'Phone', 'Industry', 'Address', 'Website', 'Google Maps Link'];
+                const headers = ['title', 'rating', 'reviews', 'address', 'address', 'industry', 'web'];
                 const headerRow = document.createElement('tr');
                 headers.forEach(headerText => {
                     const header = document.createElement('th');
                     header.textContent = headerText;
                     headerRow.appendChild(header);
                 });
+                
                 resultsTable.appendChild(headerRow);
 
-                // Add new results to the table
                 if (!results || !results[0] || !results[0].result) return;
-                results[0].result.forEach(function(item) {
-                    var row = document.createElement('tr');
-                    ['title', 'rating', 'reviewCount', 'phone', 'industry', 'address', 'companyUrl', 'href'].forEach(function(key) {
-                        var cell = document.createElement('td');
-                        
-                        if (key === 'reviewCount' && item[key]) {
-                            item[key] = item[key].replace(/\(|\)/g, ''); 
-                        }
-                        
-                        cell.textContent = item[key] || ''; 
-                        row.appendChild(cell);
-                    });
+                
+                const toUseData = results[0].result;
+
+                toUseData.forEach(function(item) {
+                    const row = document.createElement('tr');
+                    headers.forEach(key => {
+                        const cell = document.createElement('td');
+                        cell.textContent = item[key] || '';
+                        row.appendChild(cell); 
+                    })
                     resultsTable.appendChild(row);
                 });
 
@@ -67,12 +68,13 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         downloadCsvButton.addEventListener('click', function() {
-            var csv = tableToCsv(resultsTable); 
-            var filename = filenameInput.value.trim();
+            const csv = tableToCsv(resultsTable); 
+            const filename = filenameInput.value.trim();
             if (!filename) {
                 filename = 'google-maps-data.csv'; 
             } else {
-                filename = filename.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '.csv';
+                const NON_ALPHANUMERIC_REGEX = /[^a-z0-9]/gi;
+                filename = filename.replace(NON_ALPHANUMERIC_REGEX, '_').toLowerCase() + '.csv';
             }
             downloadCsv(csv, filename); 
         });
@@ -82,92 +84,59 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function scrapeData() {
-    var links = Array.from(document.querySelectorAll('a[href^="https://www.google.com/maps/place"]'));
+    const links = Array.from(document.querySelectorAll('a[href^="https://www.google.com/maps/place"]'));
     return links.map(link => {
-        var container = link.closest('[jsaction*="mouseover:pane"]');
-        var titleText = container ? container.querySelector('.fontHeadlineSmall').textContent : '';
-        var rating = '';
-        var reviewCount = '';
-        var phone = '';
-        var industry = '';
-        var address = '';
-        var companyUrl = '';
-
-        // Rating and Reviews
+        const container = link.closest('[jsaction*="mouseover:pane"]');
+        const title = container ? container.querySelector('.fontHeadlineSmall').textContent : '';
+        let rating = '';
+        let reviews = ''
+        let address = '';
+        let phone = ''
+        let industry = ''
+        let web = ''
+        
         if (container) {
-            var roleImgContainer = container.querySelector('[role="img"]');
-            
-            if (roleImgContainer) {
-                var ariaLabel = roleImgContainer.getAttribute('aria-label');
-            
-                if (ariaLabel && ariaLabel.includes("stars")) {
-                    var parts = ariaLabel.split(' ');
-                    var rating = parts[0];
-                    var reviewCount = '(' + parts[2] + ')'; 
-                } else {
-                    rating = '0';
-                    reviewCount = '0';
-                }
-            }
-        }
+            const personalDataMinimumRootEl= container.querySelector('div.fontBodyMedium');
+            if(!personalDataMinimumRootEl) return;
 
-        // Address and Industry
-        if (container) {
-            var containerText = container.textContent || '';
-            var addressRegex = /\d+ [\w\s]+(?:#\s*\d+|Suite\s*\d+|Apt\s*\d+)?/;
-            var addressMatch = containerText.match(addressRegex);
+            const ratingAndReviewContainer = personalDataMinimumRootEl.querySelector('span[role="img"]');
+            const ratingAndReviewContainerSpans = [...ratingAndReviewContainer.querySelectorAll('span')];
+            rating = ratingAndReviewContainerSpans[0].textContent;
+            const parenthesisRegex = /\(|\)/g;
+            reviews = ratingAndReviewContainerSpans[ratingAndReviewContainerSpans.length - 2].textContent.replace(parenthesisRegex, ''); 
+    
+            const industryAddressPhoneDivsContainer = [...personalDataMinimumRootEl.querySelectorAll(':scope > div')]
+            const addressSpans = industryAddressPhoneDivsContainer[3].querySelectorAll('div:first-child > span')
+            address = addressSpans[addressSpans.length - 1].querySelector('span:last-child').textContent
 
-            if (addressMatch) {
-                address = addressMatch[0];
+            phone = industryAddressPhoneDivsContainer[3].querySelector('div:last-child > span:last-child > span:last-child').textContent
 
-                // Extract industry text based on the position before the address
-                var textBeforeAddress = containerText.substring(0, containerText.indexOf(address)).trim();
-                var ratingIndex = textBeforeAddress.lastIndexOf(rating + reviewCount);
-                if (ratingIndex !== -1) {
-                    // Assuming industry is the first significant text after rating and review count
-                    var rawIndustryText = textBeforeAddress.substring(ratingIndex + (rating + reviewCount).length).trim().split(/[\r\n]+/)[0];
-                    industry = rawIndustryText.replace(/[·.,#!?]/g, '').trim();
-                }
-                var filterRegex = /\b(Closed|Open 24 hours|24 hours)|Open\b/g;
-                address = address.replace(filterRegex, '').trim();
-                address = address.replace(/(\d+)(Open)/g, '$1').trim();
-                address = address.replace(/(\w)(Open)/g, '$1').trim();
-                address = address.replace(/(\w)(Closed)/g, '$1').trim();
-            } else {
-                address = '';
-            }
-        }
+            industry = industryAddressPhoneDivsContainer[3].querySelector('div:first-child > span:first-child > span').textContent
 
-        // Company URL
-        if (container) {
-            var allLinks = Array.from(container.querySelectorAll('a[href]'));
-            var filteredLinks = allLinks.filter(a => !a.href.startsWith("https://www.google.com/maps/place/"));
+            // company urls
+            const allLinks = Array.from(container.querySelectorAll('a[href]'));
+            const GOOGLE_MAPS_PLACE_LINK = "https://www.google.com/maps/place/"
+            const filteredLinks = allLinks.filter(a => !a.href.startsWith(GOOGLE_MAPS_PLACE_LINK));
             if (filteredLinks.length > 0) {
-                companyUrl = filteredLinks[0].href;
+                web = filteredLinks[0].href;
             }
-        }
 
-        // Phone Numbers
-        if (container) {
-            var containerText = container.textContent || '';
-            var phoneRegex = /(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/;
-            var phoneMatch = containerText.match(phoneRegex);
-            phone = phoneMatch ? phoneMatch[0] : '';
         }
-
+        
         // Return the data as an object
         return {
-            title: titleText,
-            rating: rating,
-            reviewCount: reviewCount,
-            phone: phone,
-            industry: industry,
-            address: address,
-            companyUrl: companyUrl,
-            href: link.href,
+            title,
+            rating,
+            reviews,
+            address,
+            phone,
+            industry,
+            web
         };
     });
 }
+
+
 
 // Convert the table to a CSV string
 function tableToCsv(table) {
@@ -198,3 +167,4 @@ function downloadCsv(csv, filename) {
     document.body.appendChild(downloadLink);
     downloadLink.click();
 }
+
